bea73135febd01bc1a60f4fbfb15e0318ef824a3
gahyeon <pgh2031@inpix.com>
2023-08-07 17:12:22 +0900

    Movie component 2

--- src/Movie.js
+++ src/Movie.js
@@ -6,11 +6,11 @@ function Movie ({year, title, summary, poster, genres}) {
   // ※ Movie 컴포넌트에서는 state를 필요로 하지 않음 → class 컴포넌트가 될 필요 없음 → function 컴포넌트로~
   return (
     <div className="movie">
-      <img src={poster} alt={`${title} poster`} style={{ border: "4px solid darkgray"}}/>
+      <img src={poster} alt={`${title} poster`} style={{ border: "2px solid darkgray"}}/>
       <div className="movie-info">
         <h2 className="movie-title">{title} (<span title="year">{year}</span>)</h2>
-        <ul className="genres">{genres.map((genre, idx) => <li key={idx}>{genre}</li>)}</ul>
-        <p className="movie-summary">{summary}</p>
+        <ul className="movie-genres">{genres.map((genre, idx) => <li key={idx}>{genre}</li>)}</ul>
+        <p className="movie-summary">{summary.slice(0, 150)}...</p>
       </div>
 
     </div>
