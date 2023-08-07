0abedb7bdfab90d2c30a1aae7356a92288a311de
gahyeon <pgh2031@inpix.com>
2023-08-07 18:29:08 +0900

    add router

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Movie App 2023
 
 React JS Fundamentals Course 
+
+## node version
+14.21.3
+
+## npm version
+6.14.18
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import App3 from './App3';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// ※ StrictMode일 경우, 링크 클릭 시 url은 바뀌지만 화면 이동이 일어나지 않는 이슈 있음.
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App3 />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App3 />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
